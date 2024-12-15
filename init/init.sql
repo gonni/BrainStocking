551bba7d66d5d5d4f8ad3f873de24119d3a10db4
gonni <gonni21c@gamil.com>
2024-12-16 07:44:35 +0900

    daily commit

--- init/init.sql
+++ init/init.sql
@@ -1,10 +1,12 @@
 
 create table STOCK_MIN_VOLUME (
-    ts_code varchar(10),
+	item_code varchar(12) not null,
+    ts_code varchar(16) not null,
     fixed_price int,
     sell_amt int,
     buy_amt int,
-    volume int
+    volume int,
+    primary key (item_code, ts_code)
 );
 
 create table STOCK_MIN_VOLUME (
@@ -13,4 +15,11 @@ create table STOCK_MIN_VOLUME (
     sell_amt int,
     buy_amt int,
     volume int
+);
+
+create table STOCK_INFO (
+	ITEM_CODE varchar(12) not null primary key,
+	ITEM_NAME varchar(64) not null,
+	LAST_SYNCED timestamp,
+	CRAWL_STATUS int
 );
